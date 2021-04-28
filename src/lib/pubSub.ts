646d8e1c470e07575c2e7544366813b06852de2a
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2021-04-28 13:56:05 -0300

    Update pubSub.ts

--- src/lib/pubSub.ts
+++ src/lib/pubSub.ts
@@ -1,38 +1,37 @@
 export type PubSub<Payload> = {
-  dispatch: (payload: Payload) => void;
-  subscribe: (callback: (payload: Payload) => void) => () => void;
-  dispose: () => void;
-};
+  dispatch: (payload: Payload) => void
+  subscribe: (callback: (payload: Payload) => void) => () => void
+  dispose: () => void
+}
 
-// TODO: remove this and replace with streams
 export const createPubSub = <Payload>(): PubSub<Payload> => {
-  let subscriptions: Array<(payload: Payload) => void> = [];
+  let subscriptions: Array<(payload: Payload) => void> = []
 
   const subscribe = (callback: (payload: Payload) => void) => {
-    subscriptions.push(callback);
+    subscriptions.push(callback)
 
     const unsubscribe = () => {
-      const index = subscriptions.indexOf(callback);
+      const index = subscriptions.indexOf(callback)
       // mutation!
-      index !== -1 && subscriptions.splice(index, 1);
-    };
+      index !== -1 && subscriptions.splice(index, 1)
+    }
 
-    return unsubscribe;
-  };
+    return unsubscribe
+  }
 
   const dispatch = (payload: Payload) => {
     subscriptions.forEach((callback) => {
-      callback(payload);
-    });
-  };
+      callback(payload)
+    })
+  }
 
   const dispose = () => {
-    subscriptions = [];
-  };
+    subscriptions = []
+  }
 
   return {
     subscribe,
     dispatch,
     dispose,
-  };
-};
+  }
+}
